9176ab934e5a5facf47dd8f69b50e9b3a93c2b00
Roman Glukhov <romangluxov51@gmail.com>
2025-07-28 14:33:23 +0200

    START_OF_PROJECT --- V.0.01

---
+++ main.py
@@ -0,0 +1,84 @@
+from xml.etree.ElementTree import parse
+
+from config import TOKEN
+from telebot import TeleBot
+from telebot import types
+from telebot.types import ReplyKeyboardMarkup
+import text_messages
+
+bot = TeleBot(token=TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    # Создаём клавиатуру
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    # Добавляем кнопки по порядку, как в твоем скриншоте
+    btn1 = types.KeyboardButton("УНИВЕРСИТЕТЫ")
+    btn2 = types.KeyboardButton("Создатели")
+    btn3 = types.KeyboardButton("Добавить свои материалы")
+
+
+    # Каждая кнопка на отдельной строке
+    keyboard.add(btn1)
+    keyboard.add(btn2)
+    keyboard.add(btn3)
+
+    # Отправляем сообщение с клавиатурой
+    bot.send_message(message.chat.id, f"{text_messages.start_message}", reply_markup=keyboard)
+
+reply_keyboard_subject_czu = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+for item in text_messages.list_sub_czu:
+    reply_keyboard_subject_czu.add(item)
+
+reply_keyboard_univers = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+for item in text_messages.list_of_universetes:
+    reply_keyboard_univers.add(item)
+
+reply_keyboard_primers = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+for item in text_messages.list_of_primers:
+    reply_keyboard_primers.add(item)
+
+
+@bot.message_handler(func=lambda message: True)
+def check_button(message):
+    match (message.text):
+        case "УНИВЕРСИТЕТЫ":
+            bot.reply_to(message, "Выберете университет:", reply_markup=reply_keyboard_univers)
+        case "Создатели":
+            bot.reply_to(message, "Создатели")
+        case "Добавить свои материалы":
+            bot.reply_to(message, text="<a href='https://www.google.com/'>123</a>", parse_mode="HTML")
+        case "Česká zemědělská univerzita v Praze ČZU":
+            bot.reply_to(message, "Česká zemědělská univerzita v Praze ČZU", reply_markup=reply_keyboard_subject_czu)
+        case "Vysoká Škola Ekonomická v Praze (VŠE)":
+            bot.reply_to(message, f"{text_messages.vse_vyber}", reply_markup=reply_keyboard_primers)
+        case "Algoritmizace":
+            bot.reply_to(message, "АЛГОРИТМИЗАЦЕ --- ИНФОРМАЦИЯ")
+        case "Angličtina ve výpočetní technice - B1":
+            bot.reply_to(message, "АНГЛИЙСКИЙ --- ИНФОРМАЦИЯ")
+        case "Architektura počítačů":
+            bot.reply_to(message, "АРХИТЕКТУРА --- ИНФОРМАЦИЯ")
+        case "Matematika":
+            bot.reply_to(message, "МАТЕМАЦИЯ --- ИНФОРМАЦИЯ")
+        case "Objektové modelování":
+            bot.reply_to(message, "ОБЪЕКТЫ --- ИНФОРМАЦИЯ")
+        case "Programming in Python":
+            bot.reply_to(message, "ПЯТОН --- ИНФОРМАЦИЯ")
+        case "Programovací jazyk C++":
+            bot.reply_to(message, "ПРОГРАММИРОВАНИЕ --- ИНФОРМАЦИЯ")
+        case "Psychologie osobnosti a sociální psychologie":
+            bot.reply_to(message, "ПСИХОЛОГИЯ --- ИНФОРМАЦИЯ")
+        case "Teorie řízení organizačního systému":
+            bot.reply_to(message, "ТРОС --- ИНФОРМАЦИЯ")
+        case "Úvod do principu počítačů":
+            bot.reply_to(message, "УПП --- ИНФОРМАЦИЯ")
+
+
+
+
+
+
+bot.polling()
+
---
+++ text_messages.py
@@ -0,0 +1,24 @@
+start_message = "ВВОДНОЕ СООБЩЕНИЕ + ВЫБОР УНИВЕРА"
+
+list_of_universetes = ["Česká zemědělská univerzita v Praze ČZU", "Vysoká Škola Ekonomická v Praze (VŠE)"]
+
+list_sub_czu = [
+    "Algoritmizace",
+    "Angličtina ve výpočetní technice - B1",
+    "Architektura počítačů",
+    "Matematická logika a grafy",
+    "Matematika",
+    "Objektové modelování",
+    "Programming in Python",
+    "Programovací jazyk C++",
+    "Psychologie osobnosti a sociální psychologie",
+    "Teorie řízení organizačního systému",
+    "Úvod do principu počítačů"
+]
+
+list_of_primers = ["Пример1", "Пример2", "Пример3", "Пример4", "Пример5", "Пример6", "Пример7", "Пример8", "Пример9"]
+
+czu_vyber = "ВЫ ВЫБРАЛИ ЧЗУ --- КРАТКО ПРО УНИВЕР --- ВЫБОР ПРЕДМЕТОВ"
+vse_vyber = "ВЫ ВЫБРАЛИ ВШЭ --- КРАТКО ПРО УНИВЕР --- ВЫБОР ПРЕДМЕТОВ"
+
+
