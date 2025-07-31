0182cc494c87e420ba0fb1a29c3cbbb716f389ee
Roman Glukhov <romangluxov51@gmail.com>
2025-07-31 20:25:43 +0200

    Update text_messages.py --- DANIS

--- text_messages.py
+++ text_messages.py
@@ -15,10 +15,22 @@ list_sub_czu = [
     "Teorie řízení organizačního systému",
     "Úvod do principu počítačů"
 ]
+list_sub_vse = [
+    "Informační a komunikační technologie",
+    "Matematické základy informatiky",
+    "Angličtina pro ekonomická studia 1 (B2/C1)",
+    "Umělá inteligence a její aplikace",
+    "Matematika",
+    "Kvantitativní management",
+    "Teorie her a rozhodování",
+    "Matematika 2",
+    "Základy marketingu pro informatiky a statistiky",
+    "Datové minimum",
+    "UX design",
+    "Právo"
+]
 
 list_of_primers = ["Пример1", "Пример2", "Пример3", "Пример4", "Пример5", "Пример6", "Пример7", "Пример8", "Пример9"]
 
 czu_vyber = "ВЫ ВЫБРАЛИ ЧЗУ --- КРАТКО ПРО УНИВЕР --- ВЫБОР ПРЕДМЕТОВ"
 vse_vyber = "ВЫ ВЫБРАЛИ ВШЭ --- КРАТКО ПРО УНИВЕР --- ВЫБОР ПРЕДМЕТОВ"
-
-
